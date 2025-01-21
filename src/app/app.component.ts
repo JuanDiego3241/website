829dec512e517fd6f61a9d53268b6962496edfaa
JuanDiego3241 <jcortesga@unal.edu.co>
2025-01-21 17:29:29 -0500

    rework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, // Esto indica que es un componente standalone
+  imports: [CommonModule], // Importa CommonModule aquí
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'website';
+  showCurtain = true;
+
+  @ViewChild('curtainDiv') curtainDiv!: ElementRef;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  changeOpacity() {
+    if (this.showCurtain) {
+      this.curtainDiv.nativeElement.classList.add('fade-out');
+      setTimeout(() => {
+        this.showCurtain = false;
+        this.cdr.detectChanges(); // Forzar la detección de cambios
+      }, 1000);
+    } else {
+      this.showCurtain = true;
+      this.curtainDiv.nativeElement.classList.remove('fade-out');
+      this.cdr.detectChanges(); // Forzar la detección de cambios
+    }
+  }
 }
